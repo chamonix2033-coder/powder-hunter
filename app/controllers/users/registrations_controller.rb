25130090afa089a6377191ee062df03815827d8d
sasaya <sasayasu3939@gmail.com>
2026-03-08 21:27:12 +0900

    Fix email notification save bug by using standard Rails update to bypass Devise filtering

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,25 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_account_update_params, only: [ :update ]
+
   protected
 
+  def configure_account_update_params
+    # account_update アクション時に email_notifications を許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email_notifications ])
+  end
+
   def update_resource(resource, params)
-    # パスワードを変更しようとしている場合のみ、現在のパスワード入力を要求する
     if params[:password].present? || params[:password_confirmation].present?
+      # パスワード変更がある場合は Devise 標準のパスワード確認付き更新
       resource.update_with_password(params)
     else
-      # メール通知の変更や、単なるパスワード以外の変更の場合は
-      # 現在のパスワードなしで更新を許可する
+      # パスワード変更がない場合、パスワード関連と current_password を除外して直接 update を呼ぶ
+      # これにより validation エラー（Password can't be blank）を回避し、
+      # かつ Devise の独自フィルターを回避して email_notifications を確実に保存する
+      params.delete(:password)
+      params.delete(:password_confirmation)
       params.delete(:current_password)
-      resource.update_without_password(params)
+      resource.update(params)
     end
   end
 end
